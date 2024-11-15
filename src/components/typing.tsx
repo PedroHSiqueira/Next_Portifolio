37ca108ab0ceccc0ca0e5b67af92908a50ebb816
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-11-14 23:35:15 -0300

    Add Dashboard With Api project to portifolio

--- src/components/typing.tsx
+++ src/components/typing.tsx
@@ -15,11 +15,11 @@ export function Typing() {
       <div className="flex flex-col justify-around md:flex-row">
         <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
           <h1 className="text-white mb-4 text-xl sm:text-2xl lg:text-4xl lg:leading-normal font-extrabold">
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-yellow-600">Olá, eu sou</span>
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-yellow-600">Olá, Como vai?</span>
             <br></br>
-            <TypeAnimation sequence={["Pedro Siqueira", 1000, "Desenvolvedor FullStack", 1000, "Desenvolvedor Mobile", 1000]} wrapper="span" speed={50} repeat={Infinity} />
+            <TypeAnimation sequence={["Sou o Pedro Siqueira", 1000, "Sou Dev Backend", 1000]} wrapper="span" speed={50} repeat={Infinity} />
           </h1>
-            <p className="text-[#ADB7BE] text-xs sm:text-lg mb-6">Desenvolvedor apaixonado por criar soluções eficientes.</p>
+            <p className="text-[#ADB7BE] text-xs sm:text-lg mb-6">Mas possuo experiência com desenvolvimento Fullstack e Mobile</p>
         </div>
         <div className=" flex items-center justify-center col-span-4 place-self-center mt-4 lg:mt-0">
           <div className="w-4/6">{View}</div>
